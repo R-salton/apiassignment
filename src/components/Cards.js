2407d019a7c20721054906aaf4823ca129b4f694
R-salton <ringusa22@gmail.com>
2023-09-09 13:26:14 +0200

    Secod Commit

---
+++ src/components/Cards.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Button,
+ 
+} from "@material-tailwind/react";
+import axios from 'axios';
+import styled  from 'styled-components';
+import { Link } from 'react-router-dom';
+
+
+ 
+
+export default function Cards() {
+
+const [data,setData] = useState([]);
+ 
+// Fetch all Beers
+
+useEffect(() => {
+  async function fetchData(){
+    try {
+      const response = await axios.get("https://api.punkapi.com/v2/beers");
+     
+      setData(response.data)
+     
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  fetchData();
+  
+}, [data]);
+
+
+
+// console.log(data);
+
+  return (<>
+
+<Link to="/search">
+  
+          <Button size="sm" className="!absolute right-1 top-1 rounded" type='submit'>
+            Search
+          </Button>
+</Link>
+     
+        
+          
+
+    <MainContent>
+     
+    {data.map((data) =>{
+      return(
+       
+
+       
+    <Card key={data.id} className="w-[400px] h-[300px] max-w-[48rem] flex-row">
+      <CardHeader
+        shadow={false}
+        floated={false}
+        className="m-0 w-[90px] shrink-0 rounded-r-none"
+      >
+        <img
+          src={data.image_url}
+          alt={data.tagline}
+          className="h-[8x] w-[70px] object-cover  m-2"
+        />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h6" color="gray" className="mb-4 uppercase">
+          Beer
+        </Typography>
+        <Typography variant="h4" color="blue-gray" className="mb-2">
+          {data.name}
+        </Typography>
+        <Typography color="gray" className="mb-8 font-normal text-xs">
+         {data.description}
+        </Typography>
+        <a href="#" className="inline-block">
+          
+             
+        </a>
+      </CardBody>
+    </Card>
+       
+      )
+    })}
+  
+
+      
+    </MainContent>
+  </>
+  )
+}
+
+const MainContent =styled.div`
+
+width: 100vw;
+
+display: grid;
+  
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: repeat(5, 1fr);
+gap: 2rem;
+padding: 45px 45px 20px 45px;
+background-color: rgba(0,0,0,0.2);
+
+@media(max-width: 768px){
+  grid-template-columns: repeat(1, 1fr);
+  margin-left:60px;
+}
+
+`;
+
+
+
---
+++ src/components/Search.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import  { useEffect, useState } from 'react'
+import {
+ 
+  Button,
+  Input,
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Alert,
+} from "@material-tailwind/react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+function Search() {
+
+    const [beerId, setBeerId] = useState('');
+    const [beerInfo,setBeerdata] = useState(null)
+    
+   
+
+const handleSearch = async(e)=>{
+
+    e.preventDefault();
+    try {
+        const beer = await axios.get(`https://api.punkapi.com/v2/beers/${beerId}`);
+        const data = await beer.data
+        
+      setBeerdata(data[0]);
+      console.log(beerInfo);
+       
+      
+      
+        
+ 
+        
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+ 
+useEffect(()=>{
+
+    // Clear Beer Data
+    setBeerdata(null)
+},[beerId])
+
+ 
+ 
+
+
+
+  return (
+    <div>
+    <Link to="/">
+  
+  <Button size="sm" className="!absolute right-1 top-1 rounded" type='submit'>
+    Home
+  </Button>
+</Link>
+<form onSubmit={handleSearch}>
+
+
+     <div className="relative flex w-full gap-2 md:w-max m-auto mt-16 justify-center">
+          <Input
+            type="search"
+            label="Beer Id..."
+            className="pr-20"
+            value={beerId}
+            containerProps={{
+              className: "min-w-[288px]",
+            }}
+            onChange={(e) =>setBeerId(e.target.value)}
+          />
+          <Button size="sm" className="!absolute right-1 top-1 rounded" type='submit'>
+            Search
+          </Button>
+          </div>
+</form>
+
+<br />
+{beerInfo ? 
+<>
+
+    <Alert color="green" className='w-[30rem] mx-auto'>We Got Your favorite Beer!...</Alert>
+    <Card className="w-full max-w-[48rem] flex-row">
+      <CardHeader
+        shadow={false}
+        floated={false}
+        className="m-0 w-2/5 shrink-0 rounded-r-none"
+      >
+        <img
+          src={beerInfo.image_url}
+          alt="##"
+          className="h-[350px] w-[90px] object-cover mx-auto"
+        />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h6" color="gray" className="mb-4 uppercase">
+         Beer
+        </Typography>
+        <Typography variant="h4" color="blue-gray" className="mb-2">
+  {beerInfo.name}
+        </Typography>
+        <Typography color="gray" className="mb-8 font-normal">
+          {beerInfo.description}
+        </Typography>
+        
+      </CardBody>
+    </Card>
+    </>
+
+
+: <Alert color="red" className='w-[30rem] mx-auto'>Type Beer Id we can Find it for you....</Alert>}
+
+
+
+    </div>
+  )
+}
+
+export default Search
